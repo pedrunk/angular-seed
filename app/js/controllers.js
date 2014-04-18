bb438e7bbe6b4b25652ad51864f994f3ddf4b6ce
Pedro Marcano <pmarcano@spinplug.com>
2014-04-18 11:10:33 -0430

    test1

--- app/js/controllers.js
+++ app/js/controllers.js
@@ -4,8 +4,8 @@
 
 angular.module('myApp.controllers', [])
   .controller('MyCtrl1', [function() {
-
+  	console.log('controller1');
   }])
   .controller('MyCtrl2', [function() {
-
+  		console.log('controller1');
   }]);
